6df9fa7eae5e5d206a63720d1f25b374ef71ee67
liw <wei.li25@hpe.com>
2016-02-22 19:34:42 +0800

    change for file upload

--- coo-print/app/controllers/Application.scala
+++ coo-print/app/controllers/Application.scala
@@ -3,11 +3,33 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.twirl.api._
+import play.api.libs.json._
 
 class Application extends Controller {
 
   def index = Action {
+    Ok(views.html.index("Your new application is ready."));
+  }
+  
+  
+  def main = Action {
     Ok(views.html.main("Your new application is ready.")(Html("<h1>hihihi</h1>")))
+  }
+  
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body.file("files").map { file =>
+      import java.io.File
+      val filename = file.filename
+      val contentType = file.contentType
+      file.ref.moveTo(new File(s"/tmp/$filename"))
+      val json = Json.parse("""
+        { "files":[{"name":"abc"}] }        
+        """)
+      Ok(json)
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing(
+        "error" -> "Missing file")
+    }
   }
 
 }
--- coo-print/public/javascripts/main.js
+++ coo-print/public/javascripts/main.js
@@ -1,3 +1,41 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+$(function () {
+    'use strict';
+    $('#fileupload').fileupload({
+        url: 'upload',
+        dataType: 'json',
+        add: function (e, data) {
+            var progressBar = $('<div class="progress"></div>');            
+            progressBar.appendTo('#files');
+            progressBar.append($('<div class="progress-bar progress-bar-success"></div>').text(data.files[0].name));
+        	data.context = progressBar;
+        	data.submit();
+        },
+        done: function (e, data) {
+        	/*
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo('#files');
+            });\*/
+        },
+        
+        progress: function (e, data) {
+            var progress = parseInt(data._progress.loaded / data._progress.total * 100, 10);
+            data.context.each(function() {
+            	$(this).find('.progress-bar').css(
+                        'width',
+                        progress + '%'
+                );	
+            });
+
+
+        },
+        /*
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#progress .progress-bar').css(
+                'width',
+                progress + '%'
+            );
+        }*/
+    }).prop('disabled', !$.support.fileInput)
+        .parent().addClass($.support.fileInput ? undefined : 'disabled');
+});
